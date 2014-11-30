114b70de3ad4b542b75184785b65f9c0c1da7226
miremire19 <miremire19@gmail.com>
2014-11-30 21:51:25 +0300

    initial import of files

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function() {
+    var duration = 3000;
+    $(".next").on("click", function() {
+        switch (space.screen) {
+            case "loading":
+                break;
+            case "welcome":
+                $('#welcome').hide();
+                $('#angle').show();
+                $(".back").show();
+                space.screen = "angle";
+                break;
+            case "angle":
+                $('#angle').hide();
+                $('#views').show();
+                $("#views table").css("display", "none");
+                $("#" + space.angle + "-arrangement").css("display", "block");
+                space.screen = "views";
+                break;
+            case "views":
+                //space.screen="";
+        }
+    });
+    $(".back").on("click", function() {
+        switch (space.screen) {
+            case "loading":
+                break;
+            case "welcome":
+                break;
+            case "angle":
+                $('#angle').hide();
+                $('#welcome').show();
+                $(".back").hide();
+                space.screen = "welcome";
+                break;
+            case "views":
+                $('#views').hide();
+                $("#angle").show();
+                space.screen = "angle";
+        }
+    });
+    $("#angle div").on("click", function() {
+        $("#angle div").css("color", "#000");
+        $(this).css("color", "#f60");
+        space.angle = $(this).attr("id");
+        setTimeout(function() {
+            $('#angle').hide();
+            $('#views').show();
+            $("#views table").css("display", "none");
+            $("#" + space.angle + "-arrangement").css("display", "block");
+            space.screen = "views";
+        }, 200);
+    });
+    $("#views table div").on("click", function() {
+        if ($(this).attr("class") == "normal")
+            $(this).attr("class", "selected");
+        else
+            $(this).attr("class", "normal");
+    });
+});
